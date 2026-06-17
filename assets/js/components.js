2454d2387e4c60e9755a9e59978fad3249bb34ca
diegodavidextremo <diegodavidextremo@gmail.com>
2026-06-17 16:04:56 +0200

    Corrige i18n y reubica escalada clásica en vertical

--- assets/js/components.js
+++ assets/js/components.js
@@ -134,14 +134,14 @@
   if (!document.querySelector('link[data-noext-i18n]')) {
     const languageStyles = document.createElement('link');
     languageStyles.rel = 'stylesheet';
-    languageStyles.href = 'assets/css/i18n.css?v=20260615-2';
+    languageStyles.href = 'assets/css/i18n.css?v=20260617-2';
     languageStyles.dataset.noextI18n = 'styles';
     document.head.appendChild(languageStyles);
   }
 
   if (!document.querySelector('script[data-noext-i18n]')) {
     const languageScript = document.createElement('script');
-    languageScript.src = 'assets/js/i18n.js?v=20260616-2';
+    languageScript.src = 'assets/js/i18n.js?v=20260617-2';
     languageScript.dataset.noextI18n = 'runtime';
     document.head.appendChild(languageScript);
   }
