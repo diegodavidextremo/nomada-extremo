a352b049b5c830487ce896ffba2c8da4164f7b20
diegodavidextremo <diegodavidextremo@gmail.com>
2026-06-18 16:29:38 +0200

    Corrige traduccion multidioma de material

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -3,7 +3,7 @@
 
   const supported = ['es', 'en', 'fr', 'de', 'it', 'pt'];
   const storageKey = 'noext-language';
-  const catalogVersion = '20260617-6';
+  const catalogVersion = '20260618-1';
   const page = (location.pathname.split('/').pop() || 'index.html').replace('.html', '') || 'index';
   const ignoredTags = new Set(['SCRIPT', 'STYLE', 'NOSCRIPT', 'TEMPLATE', 'CODE', 'PRE']);
   const originalText = new WeakMap();
