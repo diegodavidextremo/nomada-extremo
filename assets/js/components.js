2e731b4950965f6b512cb6b7f764abeb3a42cc43
diegodavidextremo <diegodavidextremo@gmail.com>
2026-06-19 14:00:38 +0200

    Traduce bloque Criterio Nomada del blog

--- assets/js/components.js
+++ assets/js/components.js
@@ -134,14 +134,14 @@
   if (!document.querySelector('link[data-noext-i18n]')) {
     const languageStyles = document.createElement('link');
     languageStyles.rel = 'stylesheet';
-    languageStyles.href = 'assets/css/i18n.css?v=20260619-1';
+    languageStyles.href = 'assets/css/i18n.css?v=20260619-2';
     languageStyles.dataset.noextI18n = 'styles';
     document.head.appendChild(languageStyles);
   }
 
   if (!document.querySelector('script[data-noext-i18n]')) {
     const languageScript = document.createElement('script');
-    languageScript.src = 'assets/js/i18n.js?v=20260619-1';
+    languageScript.src = 'assets/js/i18n.js?v=20260619-2';
     languageScript.dataset.noextI18n = 'runtime';
     document.head.appendChild(languageScript);
   }
